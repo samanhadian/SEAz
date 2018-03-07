9c0bad146fa39bd8b71b5a7b46a936d76bece036
Sahadian <samiiiii1375@gmail.com>
2018-03-07 15:37:23 +0330

    subtract added

--- src/BasicMath.java
+++ src/BasicMath.java
@@ -4,9 +4,18 @@ class BasicMath
     public int sum(int a, int b)
     {
         return a + b;
+		
     }
+	
+	
 
     // TODO: subtract
+    public int subtract(int a, int b)
+    {
+        return a - b;
+		
+    }
+	
 	// TODO: multiply
 	// TODO: divide
 }
