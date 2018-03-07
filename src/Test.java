eeecf49036cf55a84f602afbced6cebce5d44c97
Sahadian <samiiiii1375@gmail.com>
2018-03-07 15:49:08 +0330

    multiply added

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,7 @@ class Test
     public static void main(String[] args)
     {
         testSum();
+		testMultiply();
     }
 
     private static void testSum()
@@ -19,6 +20,22 @@ class Test
         else
         {
             System.out.println("Sum: Failed, expected = " + expected + ", result = " + result);
+        }
+    }
+    private static void testMultiply()
+    {
+        int a = 5;
+        int b = 6;
+        int expected = 30;
+        BasicMath bm = new BasicMath();
+        int result = bm.multiply(a, b);
+        if (result == expected)
+        {
+            System.out.println("Sum: OK");
+        }
+        else
+        {
+            System.out.println("multiply: Failed, expected = " + expected + ", result = " + result);
         }
     }
 }
